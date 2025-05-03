0e23da6f29a12a7b3c240d4a6306b860392e05f2
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-05-04 00:24:46 +0600

    folder stracturing for frontent project

---
+++ SkillStack-Frontend/src/components/student/CoursesSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoursesSection = () => {
+  return (
+    <div>
+      <h2>Courser Section</h2>
+    </div>
+  )
+}
+
+export default CoursesSection
--- SkillStack-Frontend/src/main.jsx
+++ SkillStack-Frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AppContextProvider } from "./context/AppContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
+);
