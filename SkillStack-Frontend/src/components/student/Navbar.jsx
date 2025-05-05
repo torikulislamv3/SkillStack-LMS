340bf1dd16c132420033d0e624c809c0f5335038
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-05-05 23:28:00 +0600

    navbar desiging

--- SkillStack-Frontend/src/components/student/Navbar.jsx
+++ SkillStack-Frontend/src/components/student/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-      <h2>Navbar</h2>
+      <img src={assets.skillStackLogo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
+      <div className='hidden md:flex items-center gap-5 text-gray-500'>
+          <div>
+            <button>Become Educator</button>
+            | <Link to={'/my-enrollments'}>My Enrollments</Link>
+          </div>
+      </div>
+      <div></div>
     </div>
   )
 }
