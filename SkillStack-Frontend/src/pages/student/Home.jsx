af87b90afa918fe507e4c2f3ec7f99a2ad0c8d24
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-05-13 22:55:19 +0600

    companies section design ready

--- SkillStack-Frontend/src/pages/student/Home.jsx
+++ SkillStack-Frontend/src/pages/student/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Hero from '../../components/student/Hero'
+import React from "react";
+import Hero from "../../components/student/Hero";
+import Companies from "../../components/student/Companies";
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center space-y-7 text-center'>
+    <div className="flex flex-col items-center space-y-7 text-center">
       <Hero />
+      <Companies />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
